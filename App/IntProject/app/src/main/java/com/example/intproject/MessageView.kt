551478db5e1dfaccbebbff53a49d17f7165b0434
oscar ekholm <oscarekh@kth.se>
2019-02-21 14:18:12 +0100

    fix copy/paste miss from roomview

--- App/IntProject/app/src/main/java/com/example/intproject/MessageView.kt
+++ App/IntProject/app/src/main/java/com/example/intproject/MessageView.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
 import android.widget.TextView
+//import kotlinx.android.synthetic.main.message_view.view.*
 
 class MessageView : LinearLayout{
 
     constructor(context: Context, message: String) : super(context) {
-        inflate(context, R.layout.room_view, this)
+        inflate(context, R.layout.message_view, this)
 
         val txtMessage: TextView = findViewById(R.id.txtMessage)
         txtMessage.text = message
@@ -16,11 +17,10 @@ class MessageView : LinearLayout{
 
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
         // Based on this custom view tutorial: https://medium.com/@douglas.iacovelli/the-beauty-of-custom-views-and-how-to-do-it-79c7d78e2088
-        inflate(context, R.layout.room_view, this)
-
-        val txtMessage: TextView = findViewById(R.id.txtMessage)
+        inflate(context, R.layout.message_view, this)
 
         attrs?.let {
+            val txtMessage: TextView = findViewById(R.id.txtMessage)
             val attributes = context.obtainStyledAttributes(attrs, R.styleable.MessageView)
             txtMessage.text = attributes.getString(R.styleable.MessageView_message)
 
