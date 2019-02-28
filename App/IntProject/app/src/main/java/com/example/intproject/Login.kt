6e159a66ddfdf1618e2e56e2b60fe6ad091861c9
oscar ekholm <oscarekh@kth.se>
2019-02-28 13:59:12 +0100

    add login via server

--- App/IntProject/app/src/main/java/com/example/intproject/Login.kt
+++ App/IntProject/app/src/main/java/com/example/intproject/Login.kt
@@ -5,7 +5,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.login.*
+import org.json.JSONObject
+import java.lang.Exception
 
 class Login : AppCompatActivity() {
 
@@ -14,16 +20,44 @@ class Login : AppCompatActivity() {
         setContentView(R.layout.login)
 
         btnLogin.setOnClickListener {
-            val username = edtUsername.text.toString()
-            val password = edtPassword.text.toString()
+            //val username = edtUsername.text.toString()
+            //val password = edtPassword.text.toString()
 
-            if (username == "Oscar Ekholm" && password == "hunter2") {
-                val intent = Intent(this, Rooms::class.java)
-                //intent.putExtra("identifier", "hej")
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, "Wrong login credentials!", Toast.LENGTH_SHORT).show()
-            }
+
+            val url = "http://130.229.132.61/login"
+            val queue = Volley.newRequestQueue(this)
+            val reqBody = JSONObject()
+            reqBody.put("name", edtUsername.text.toString())
+            reqBody.put("password", edtPassword.text.toString())
+
+
+            val req = JsonObjectRequest(
+                Request.Method.POST, url, reqBody,
+                Response.Listener<JSONObject> { response ->
+                    try {
+                        val success = response.getBoolean("success")
+                        val token = response.getString("token")
+                        login(success)
+                    } catch (e : Exception) {
+                        //txtDebug.text = "I Re.Lis: " + e.toString()
+                        login(false)
+                    }
+                },
+                Response.ErrorListener { error ->
+                    txtDebug.text = "I Er.Lis: " + error.message
+                })
+
+            queue.add(req)
+            queue.start()
+        }
+    }
+
+    fun login(success: Boolean) {
+        if (success) {
+            val intent = Intent(this, Rooms::class.java)
+            startActivity(intent)
+        } else {
+            Toast.makeText(this, "Wrong login credentials!", Toast.LENGTH_SHORT).show()
         }
     }
 }
