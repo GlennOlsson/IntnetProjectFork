6a545b08f98d79a54b02f56468cea1108e3fc451
Glenn Olsson <glennholsson@gmail.com>
2019-03-08 12:37:01 +0100

    Remove CORS

--- Server/index.js
+++ Server/index.js
@@ -4,18 +4,13 @@ const app = express();
 
 const httpServer = http.Server(app);
 
-const router = require('./controllers/rest.controller');
-app.use(express.json());
-app.use((res, req, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-})
-
-app.use("/", router);
-
 const socketSetup = require('./controllers/socket.controller');
 socketSetup(httpServer);
+
+const router = require('./controllers/rest.controller');
+app.use(express.json());
+
+app.use("/", router);
 
 httpServer.listen(8082, () => {
     console.log("Server running");
